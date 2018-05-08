f528d3fec920584d40fef8ae8f1c90f4ec962675
Paul <rpmstevenson@gmail.com>
2018-05-08 19:25:38 +0100

    Issues - item update, supplier update, customer new, customer update

--- controllers/customers_controller.rb
+++ controllers/customers_controller.rb
@@ -7,3 +7,31 @@ get '/dashboard/customers' do
   @customers = Customer.all
   erb(:"customers/index")
 end
+
+post '/dashboard/customers' do
+ Customer.new(params).save
+ redirect to("/dashboard/customers")
+end
+
+get '/dashboard/customers/:id/edit' do
+  @customer = Customer.find(params[:id])
+  erb(:"customers/edit")
+end
+
+post '/dashboard/customers/:id' do
+  customer = Customer.new(params)
+  customer.update
+  redirect to '/dashboard/customers'
+end
+
+post '/dashboard/customers/:id/delete' do
+  customer = Customer.find(params[:id])
+  customer.delete()
+  redirect to '/dashboard/customers'
+end
+
+
+get'/dashboard/customers/:id' do
+  @customer = Customer.find(params[:id])
+  erb(:"customers/show")
+end
